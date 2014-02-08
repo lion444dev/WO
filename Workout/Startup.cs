8f9294496c5d556859a02b4532864f581ed63611
mleon <mleon@me.com>
2014-02-08 11:44:01 -0800

    pristine

---
+++ Workout/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Workout.Startup))]
+namespace Workout
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
